477d9c331dd70928099a263227dacb73c8f28af4
李宇登 <li@maichong.it>
2016-10-21 12:01:45 +0800

    使用 seamless-immutable

--- index.js
+++ index.js
@@ -5,19 +5,23 @@
  */
 
 import wx from 'labrador';
+import immutable from 'seamless-immutable';
 import CartItem from 'al-cart-item';
 
-const { func } = wx.PropTypes;
+const { array, func } = wx.PropTypes;
 
 export default class Cart extends wx.Component {
   propTypes = {
-    onSelectList: func,
+    list: array,
+    onItemTap: func,
     onItemDelete: func,
+    onSelectList: func,
     onItemQuantitySet: func,
-    onItemTap: func
+    onOrderCreateTap: func
   };
 
   data = {
+    list: immutable([]),
     selectCount: 0,
     allSelectStatus: true,
     selectPrice: 0
@@ -25,48 +29,125 @@ export default class Cart extends wx.Component {
 
   children = {
     listItems: new wx.List(CartItem, 'list', {
-      checked: '>checked',
-      quantity: '>quantity',
-      onItemSelect: '#handleItemSelect',
-      onItemDelete: '#handleItemDelete',
-      onItemQuantitySet: '#handleItemQuantitySet',
-      onItemTap: '#handleItemTap'
+      item: '>>',
+      onSelect: '#handleItemSelect',
+      onDelete: '#handleItemDelete',
+      onQuantitySet: '#handleItemQuantitySet',
+      onTap: '#handleItemTap'
     })
   };
 
-  handleItemSelect(id, select) {
+  handleAllSelectTap() {
+
     let list = [];
+    let selectCount = 0;
     let selectPrice = 0;
+    let selectList = [];
+    let allSelectStatus = !this.data.allSelectStatus;
     this.data.list.forEach((item) => {
-      if (item.checked && item.id !== id) {
-        list.push(item.id);
-        selectPrice += item.price;
-      } else if (item.id === id) {
-        item.checked = select;
-        if (item.checked) {
-          list.push(item.id);
-          selectPrice += item.price;
-        }
+      let temp = item.asMutable();
+      if (this.data.allSelectStatus) {
+        temp.checked = false;
+      } else {
+        temp.checked = true;
+        selectCount += 1;
+        selectPrice += temp.price * temp.quantity;
+        selectList.push(temp.id);
       }
+      list.push(immutable(temp));
     });
-    let allSelectStatus = list.length === this.data.list.length;
-    this.setData({ selectCount: list.length, allSelectStatus, selectPrice });
-    this.props.onSelectList(list);
+    console.log('selectList-----', selectList);
+    this.props.onSelectList(selectList);
+    this.setData({ list, selectList, selectCount, selectPrice, allSelectStatus });
   }
 
-  handleItemDelete(id) {
-    console.log('al-cart----delete-',id);
+  handleItemSelect(component, select) {
+    let id = this.data.list[component.key].id;
+    console.log('ar-cart handleItemSelect', id, select);
+    let list = [];
+    let selectList = [];
+    let selectPrice = 0;
+    this.data.list.forEach((item) => {
+      if (item.id === id) {
+        let temp = item.asMutable();
+        temp.checked = select;
+        list.push(immutable(temp));
+        if (select) {
+          selectList.push(temp.id);
+          selectPrice += temp.price * temp.quantity;
+        }
+      } else {
+        if (item.checked) {
+          selectList.push(item.id);
+          selectPrice += item.price * item.quantity;
+        }
+        list.push(item);
+      }
+    });
+    console.log('selectList-----', selectList);
+    this.props.onSelectList(selectList);
+    let allSelectStatus = selectList.length === this.data.list.length;
+    this.setData({ list, selectList, selectCount: selectList.length, allSelectStatus, selectPrice });
+  }
+
+  handleItemDelete(component) {
+    let id = this.data.list[component.key].id;
+    console.log('al-cart----delete-', id);
     this.props.onItemDelete(id);
   }
 
-  handleItemQuantitySet(id, quantity) {
-    console.log('handleItemQuantitySet',arguments);
+  handleItemQuantitySet(component, quantity) {
+    let id = this.data.list[component.key].id;
+    console.log('al - cart handleItemQuantitySet', id);
     this.props.onItemQuantitySet(id, quantity);
   }
 
-  handleItemTap(id) {
-    console.log('al-cart tap ------',id);
+  handleItemTap(component) {
+    let id = this.data.list[component.key].id;
+    console.log('al-cart tap ------id,==', id);
     this.props.onItemTap(id);
+  }
+
+  onUpdate(props) {
+    console.log('al cart props.list', props.list);
+    let data = {
+      list: [],
+      selectCount: 0,
+      selectPrice: 0,
+      selectList: [],
+      allSelectStatus: false
+    };
+    props.list.forEach((item) => {
+      let flag = true;
+      this.data.list.forEach((temp) => {
+        if (item.id === temp.id) {
+          flag = false;
+          item.checked = temp.checked;
+          data.list.push(immutable(item));
+        }
+      });
+      if (flag) {
+        item.checked = true;
+        data.list.push(immutable(item));
+      }
+    });
+    data.list.forEach((item) => {
+      if (item.checked) {
+        data.selectList.push(item.id);
+        data.selectCount += 1;
+        data.selectPrice += item.price * item.quantity;
+      }
+    });
+    console.log('al cart list----', data.list);
+    props.onSelectList(data.selectList);
+    data.allSelectStatus = data.list.length === data.selectCount;
+    this.setData(data);
+  }
+
+  handleCreateTap() {
+    if (this.data.selectList.length > 0) {
+      this.props.onOrderCreateTap();
+    }
   }
 
   onLoad() {
@@ -87,36 +168,5 @@ export default class Cart extends wx.Component {
 
   onUnload() {
 
-  }
-
-  onUpdate(props) {
-    let list = [];
-    if (props.list && props.list.length > 0) {
-      props.list.forEach((item) => {
-        let flag = 0;
-        this.data.list.forEach((_item) => {
-          if (item.id === _item.id) {
-            flag = 1;
-            list.push({ ..._item });
-          }
-        });
-        if (flag === 0) {
-          list.push({ ...item, checked: true });
-        }
-      });
-    }
-    let selectCount = 0;
-    let selectPrice = 0;
-    let selectList = [];
-    list.forEach((item) => {
-      if (item.checked) {
-        selectList.push(item.id);
-        selectCount += 1;
-        selectPrice += item.price;
-      }
-    });
-    props.onSelectList(selectList);
-    let allSelectStatus = list.length === selectCount;
-    this.setData({ ...props, list, selectCount, allSelectStatus, selectPrice });
   }
 }
